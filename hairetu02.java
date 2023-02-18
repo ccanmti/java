eca2124d6ad336d3e927a1f580e21ac7a9d5fc31
鈴木淳之介 <jun@koaranoMacBook-Air.local>
2023-02-18 23:34:18 +0900

    hairetu

---
+++ hairetu02.java
@@ -0,0 +1,12 @@
+public class hairetu02{
+    // 配列の基本的な使い方
+    public static void main(String[] args){
+        int[] array;
+        array = new int[4];
+        array[0] = 10;
+        array[1] = 20;
+        array[2] = 40;
+        array[3] = 30;
+        System.out.println(array[2]);
+    }
+}
---
+++ hairetu03.java
@@ -0,0 +1,8 @@
+public class hairetu03{
+    public static void main(String[] args){
+        int[] array = {10,20,30};
+        for(int i =0; i < array.length; i++){
+            System.out.println(array[i]);
+        }
+    }
+}
