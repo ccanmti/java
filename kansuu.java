88222a8d0b7e60015178b8e18f46eb336cad7385
鈴木淳之介 <jun@koaranoMacBook-Air.local>
2023-02-18 22:56:19 +0900

    kansuu

---
+++ kansuu.java
@@ -0,0 +1,8 @@
+public class kansuu{
+    public static void main(String[] args){
+        hello();
+    }
+    public static void hello(){
+        System.out.println("hello");
+    } 
+}
---
+++ kansuu01.java
@@ -0,0 +1,8 @@
+public class kansuu01{
+    public static void main(String[] args){
+        menseki(10,6);
+    }
+    public static void menseki(int a,int b ){
+        System.out.println(a*b);
+    }
+}
