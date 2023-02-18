a55dea98b371c2aa7e10da803230dd2e9f80a3ea
鈴木淳之介 <jun@koaranoMacBook-Air.local>
2023-02-19 00:06:56 +0900

    hairetu

---
+++ hairetu05.java
@@ -0,0 +1,13 @@
+public class hairetu05{
+    public static void main(String[] args){
+        int[] score = {64,76,98};
+        keisan(score);
+    }
+    public static void keisan(int[] x){
+        int sum = 0;
+        for(int i = 0; i <x.length; i++ ){
+            sum += x[i];
+        }
+        System.out.println("平均点は"+ (sum/x.length));
+    }
+}
