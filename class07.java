fc5f233d74f576dd0dab9dcfdffbe51e4a6ca5c6
鈴木淳之介 <ccanmti@gmail.com>
2023-02-20 16:21:30 +0900

    aaa

---
+++ class07.java
@@ -0,0 +1,12 @@
+public class class07{
+    public static void main(String[] args){
+        String count;
+        for(int i =0; i<=10; i++){
+            System.out.print(i);
+            if(i == 10)
+                continue;
+                System.out.print(",");
+            
+        }
+    }
+}
