959bc84f9a50c07a6da4febf11e9a84d82c96b09
鈴木淳之介 <ccanmti@gmail.com>
2023-02-21 08:31:15 +0900

    java_practice

--- class07.java
+++ class07.java
@@ -1,12 +1,22 @@
 public class class07{
     public static void main(String[] args){
-        String count;
-        for(int i =0; i<=10; i++){
-            System.out.print(i);
-            if(i == 10)
-                continue;
+        int[] sum = new int[20];
+        sum[0] = 10;
+        int i = 0;
+        for(i =0; i < 20; i++){
+            sum[i]= i * 5;
+        }
+        for(i = 0;  i < 20; i++){
+            if(i < 10){
+            System.out.print(sum[i]);
+            if(!(sum[i] % 2 ==0))
                 System.out.print(",");
-            
+            }else{
+                System.out.print(sum[i]);
+                if(sum[i] % 2 ==0)
+                    System.out.print(",");
+                
+            }
         }
     }
 }
---
+++ class08.java
@@ -0,0 +1,15 @@
+public class class08{
+    public static void main(String[] args){
+        int i =0;
+        int count = 0;
+        int sum = 0;
+        for(i=0; i<11; i++){
+            System.out.print(i);
+            
+            if(i == 10)
+            continue;
+                System.out.print(",");
+            
+        }
+    }
+}
